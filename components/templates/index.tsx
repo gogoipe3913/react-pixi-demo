dba412279714276856304a7f12c9e075edc805a3
Taiki Kishiyama <tkishiya@TaikinoMacBook-Pro.local>
2022-02-18 01:49:20 +0900

    atomic design になるように構造を修正

---
+++ components/templates/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const PixiTemplate: React.FC = props => (
+  <>
+    <Head>
+      <meta title="react-pixi-demo" />
+    </Head>
+    <div>とりあえずあああ</div>
+  </>
+);
+
+export default PixiTemplate;
